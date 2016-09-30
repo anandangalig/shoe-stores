401cc418b063012eb35e2709bcbc146a49069dd5
Anand Angalig <anandangalig@gmail.com>
2016-09-30 14:35:07 -0400

    create landing page and the index route

---
+++ app/app.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+    *@backupGlobals disabled
+    *@backupStaticAttributes disabled
+    */
+    
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/Brand.php";
+    require_once __DIR__."/../src/Store.php";
+
+    $app = new Silex\Application();
+
+    $server = "mysql:host=localhost:8889;dbname=shoes";
+    $username = "root";
+    $password = "root";
+    $DB = new PDO($server, $username, $password);
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array("twig.path" => __DIR__."/../views"));
+
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
+    $app->get("/", function() use ($app) {
+        return $app["twig"]->render("index.html.twig", array('stores' => Store::getAll(), 'brands' => Brand::getAll()));
+    });
+
+
+    return $app;
+ ?>
